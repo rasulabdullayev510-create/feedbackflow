4464e9a64ca70b27321529bc3bf1eff0ab4e95be
Rasul Abdullayev <rasulabdullayev@Rasuls-MacBook-Pro.local>
2026-03-27 00:57:19 -0600

    Add CORS to allow requests from skidocyyc.ca

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const crypto = require("crypto");
 const path = require("path");
 const cron = require("node-cron");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 
